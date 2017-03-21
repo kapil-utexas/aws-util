77b957b604ea2bb2447e9eac6497d25bbbb3ce2c
kapil <kapil@utexas.edu>
2017-03-21 14:23:05 -0500

    more automation in emr environment setup

--- install_util.sh
+++ install_util.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -x -e
-# install pip & tmux
+# A basic emr-cluster env setup script 
+sudo yum -y update
 sudo yum -y install git
+git clone https://github.com/kapil-utexas/aws-util.git
+cp -r aws_utils/.vim/ .
+cp aws_utils/.vimrc
+./setup_jupyter_pyspark.sh
 #setting env variables
-export S3DISTCPJAR="/home/hadoop/lib/emr-s3distcp-1.0.jar"
 
