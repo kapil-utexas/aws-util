8bb2f30586f64e51580b661f561a7e01ce9f0eaf
kapil <kapil@utexas.edu>
2017-03-22 11:41:20 -0500

    changed install.sh

--- install_util.sh
+++ install_util.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# A basic emr-cluster env setup script 
-sudo yum -y update
+# A basic emr-cluster env setup script
+#just copy this to the emr cluster first
+#sudo yum -y update
 sudo yum -y install git
 git clone https://github.com/kapil-utexas/aws-util.git
-cp -r aws_utils/.vim/ .
-cp aws_utils/.vimrc
-./setup_jupyter_pyspark.sh
+cp -r aws-util/.vim/ .
+cp aws-util/.vimrc
+sh setup_jupyter_pyspark.sh
 #setting env variables
 
